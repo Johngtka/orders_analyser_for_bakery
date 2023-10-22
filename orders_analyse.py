fa48e679ae90212e2c81de8bf17c3e4969f24649
Johngtka <johngtka@gmail.com>
2023-10-22 15:22:46 +0200

    feat: added getting name , surname, email, orders count

--- orders_analyse.py
+++ orders_analyse.py
@@ -1,4 +1,5 @@
-import pymysql;  
+import pymysql
+import time
 
 conn = pymysql.connect(
     host='localhost',
@@ -8,16 +9,23 @@ conn = pymysql.connect(
 )
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM klijeci")
-emails = [row[0] for row in cursor.fetchall()]
-
+cursor.execute("SELECT email, name, surName FROM klijeci")
+users = cursor.fetchall()  # Pobierz wszystkie kolumny z wyników
+clock = 3
 orders_count = {}
 
-for email in emails:
+for user in users:
+    email, name, surName = user  # Wydobywanie danych użytkownika
     cursor.execute("SELECT COUNT(id) FROM zamowienia WHERE email = %s", (email,))
     orders_count[email] = cursor.fetchone()[0]
 
+for i in range(3, 0, -1):
+        print(f"Odliczanie: {i}", end='\r')  # Ustawienie end='\r' dla powrotu na początek linii
+        time.sleep(1) 
+
 for email, zamowienia in orders_count.items():
-    print(f"Email: {email}, Liczba zamówień: {zamowienia}")
+     
+    name, surName = [user[1:3] for user in users if user[0] == email][0]
+    print(f"Imię: {name}, Nazwisko: {surName}, Email: {email}, Liczba zamówień: {zamowienia}")
 
 conn.close()
